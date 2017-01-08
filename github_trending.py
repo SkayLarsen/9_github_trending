6b553562b1ee95abe4476c3dc70bb3c506dad180
SkayLarsen <SkayLarsen@gmail.com>
2017-01-08 15:59:23 +0300

    задача #9

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,18 @@
+import requests
+from datetime import date, timedelta
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_trending_repositories(top_size=20, days=7):
+    url = "https://api.github.com/search/repositories"
+    start_date = date.today() - timedelta(days)
+    params = {'q': 'created:>%s' % start_date, 'sort': 'stars', 'per_page': top_size}
+    response = requests.get(url, params)
+    return response.json()["items"]
 
-
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
 
 if __name__ == '__main__':
-    pass
+    trending_repositories = get_trending_repositories()
+    print("Список интересных репозиториев:")
+    for rep in trending_repositories:
+        print("Репозиторий {}  имеет {} звёзд, {} открытых задач, и доступен по ссылке: {}".format(
+              rep['name'], rep['stargazers_count'], rep['open_issues'], rep['html_url']))
